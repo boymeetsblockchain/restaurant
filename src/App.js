cd8fbb71c8b3e924cc113db52b3cf4823a13109f
Emmanuel uzoezie <mac@macs-MacBook-Pro.local>
2022-10-21 03:06:55 +0100

    creating the omepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import FoodComponent from './components/FoodComponent';
+import { useStateContext } from './context/ContextProvider';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const { sidebar } = useStateContext()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <div className='app-header'> */}
+        {sidebar? (
+          <div className='sidebar'>
+            <Sidebar />
+          </div>
+        ): (
+          ""
+        )}
+        <Header />
+      {/* </div> */}
+      <FoodComponent />
+      <h1>get started</h1>
     </div>
   );
 }
